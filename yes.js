56281de3ef6f61ed00b0df7157c66f1cdd04c867
Marco Chen <61860244+marcocharco@users.noreply.github.com>
2025-02-11 12:20:56 -0500

    update envelope animation to be animation on hover

--- yes.js
+++ yes.js
@@ -1,46 +1,26 @@
 $(document).ready(function() {
     var envelope = $("#envelope");
+    var btn_open = $("#open");
     var btn_reset = $("#reset");
 
-    function startVibratingInterval() {
-        setInterval(() => {
-            
-            if (!envelope.hasClass("open")) {
-                envelope.addClass("vibrating");
-
-                setTimeout(() => {
-                    envelope.removeClass("vibrating");
-                }, 800);
-            }
-        }, 3000);
-    }
-
-    startVibratingInterval();
-
     envelope.click(function() {
-        vibrateAndOpen();
+        open();
     });
-
+    btn_open.click(function() {
+        open();
+    });
     btn_reset.click(function() {
         close();
     });
 
-    function vibrateAndOpen() {
-        envelope.removeClass("vibrating");
-
-        setTimeout(function() {
-            open();
-        }, 100);
-    }
-
     function open() {
-        envelope.addClass("open").removeClass("close");
+        envelope.addClass("open")
+            .removeClass("close");
     }
 
     function close() {
-        envelope.addClass("close").removeClass("open");
-        setTimeout(() => {
-            startVibratingInterval(); // Restart vibration cycle
-        }, 1000);
+        envelope.addClass("close")
+            .removeClass("open");
     }
-});
+
+})
