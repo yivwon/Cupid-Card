63c189e33a71a60eff203ce9c7a18b4235739bae
addetude <addelue@gmail.com>
2025-02-11 04:03:54 -0500

    Envelope animation and button fix

--- yes.js
+++ yes.js
@@ -1,26 +1,46 @@
 $(document).ready(function() {
     var envelope = $("#envelope");
-    var btn_open = $("#open");
     var btn_reset = $("#reset");
 
+    function startVibratingInterval() {
+        setInterval(() => {
+            
+            if (!envelope.hasClass("open")) {
+                envelope.addClass("vibrating");
+
+                setTimeout(() => {
+                    envelope.removeClass("vibrating");
+                }, 800);
+            }
+        }, 3000);
+    }
+
+    startVibratingInterval();
+
     envelope.click(function() {
-        open();
+        vibrateAndOpen();
     });
-    btn_open.click(function() {
-        open();
-    });
+
     btn_reset.click(function() {
         close();
     });
 
+    function vibrateAndOpen() {
+        envelope.removeClass("vibrating");
+
+        setTimeout(function() {
+            open();
+        }, 100);
+    }
+
     function open() {
-        envelope.addClass("open")
-            .removeClass("close");
+        envelope.addClass("open").removeClass("close");
     }
 
     function close() {
-        envelope.addClass("close")
-            .removeClass("open");
+        envelope.addClass("close").removeClass("open");
+        setTimeout(() => {
+            startVibratingInterval(); // Restart vibration cycle
+        }, 1000);
     }
-
-})
+});
